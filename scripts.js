c77667b1bf37279efc58536976f0231096d62909
o5maks <o4x0n4s@proton.me>
2024-09-03 17:17:30 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,71 @@
+const isEmptyError = 'Please type in something';
+const invalidJSONInputError = 'Invalid JSON input';
+
+function isObject(any) {
+    return typeof any == 'object' && !Array.isArray(any);
+}
+
+function convert(obj) {
+    const keys = Array.isArray(obj) 
+        ? [...new Set(obj.flatMap((x) => Object.keys(x)))]
+        : Object.keys(obj);
+  
+    const lines = [keys];
+
+    if (Array.isArray(obj)) {
+        for (const item of obj) {
+            const row = keys.map((header) => (
+                item[header]
+            ));
+            lines.push(row);
+        }
+    } else {
+        const row = keys.map((header) => (
+            obj[header]
+        ));
+        lines.push(row);
+    }
+  
+    return lines.map((line) => (line
+        .map((char) => (
+            char ? `"${char}"` : ''
+        ))
+        .join(',')
+    )).join('\n');
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const inputElement = document.querySelector('textarea.input');
+    const parseButton = document.querySelector('button.parse');
+    const resetButton = document.querySelector('button.reset');
+    const copyButton = document.querySelector('button.copy');
+    const outputElement = document.querySelector('pre');
+
+    parseButton.onclick = function () {
+        const content = inputElement.value;
+
+        if(!content) {
+            outputElement.innerText = isEmptyError;
+            return;
+        }
+        
+        try {
+            const jsonData = JSON.parse(content);
+            const csv = convert(jsonData);
+            outputElement.innerText = csv;
+        } catch {
+            outputElement.innerText = invalidJSONInputError;
+        }
+    };
+
+    resetButton.onclick = function () {
+        inputElement.value = '';
+        outputElement.innerText = '';
+    };
+
+    copyButton.onclick = function () {
+        navigator.clipboard.writeText(outputElement.innerText);
+    };
+});
+
